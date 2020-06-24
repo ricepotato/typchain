506df7a764417b444fda43e514b7bc8f937971fa
Sukjun Sagong <ricepotato40@gmail.com>
2020-06-24 20:50:45 +0900

    ts watch

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ const gender = "male";
 
 // argument 에 ? 를 붙이면 optional parameter 로 만들 수 있다.
 // 붙이지 않으면 호출할때 parameter 가 부족하면 에러가 발생한다.
-const sayHi = (name, age, gender?) => {
-  console.log(`Hello ${name} you are ${age}. you are a ${gender}`);
+const sayHi = (name: string, age: number, gender: string): string => {
+  const message = `Hello ${name} you are ${age}. you are a ${gender}`;
+  console.log(message);
+  return message;
 };
 
-sayHi(name, age);
+sayHi(name, age, gender);
 
 export {}; // 이것이 모듈임을 표현하는 typescript 문법

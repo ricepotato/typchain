4a3b66ae396a98a72c59b2fcbe19b3f409b3480d
Sukjun Sagong <ricepotato40@gmail.com>
2020-06-24 20:54:55 +0900

    typescript interface

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,26 @@ const name = "sukjun.sagong";
 const age = 24;
 const gender = "male";
 
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "nicolas",
+  gender: "male",
+  age: 22,
+};
+
 // argument 에 ? 를 붙이면 optional parameter 로 만들 수 있다.
 // 붙이지 않으면 호출할때 parameter 가 부족하면 에러가 발생한다.
-const sayHi = (name: string, age: number, gender: string): string => {
-  const message = `Hello ${name} you are ${age}. you are a ${gender}`;
+const sayHi = (person: Human): string => {
+  const message = `Hello ${person.name} you are ${person.age}. you are a ${person.gender}`;
   console.log(message);
   return message;
 };
 
-sayHi(name, age, gender);
+sayHi(person);
 
 export {}; // 이것이 모듈임을 표현하는 typescript 문법
